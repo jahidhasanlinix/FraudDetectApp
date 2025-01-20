16066a873de718781846a842cdc2ca5bc3b16f8b
JH <36171973+jahidhasanlinix@users.noreply.github.com>
2025-01-20 02:47:37 -0600

    parq file

--- fraud_detection_app.py
+++ fraud_detection_app.py
@@ -11,7 +11,7 @@ import plotly.graph_objects as go
 from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
-# Test parquet files ok
+# Tested it again
 st.set_page_config(page_title="Credit Card Fraud Detection System", layout="wide")
 
 def load_and_preprocess_data(uploaded_file):
