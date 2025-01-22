83fd7e6eaff420e4286f46225ad36a869f761887
JH <36171973+jahidhasanlinix@users.noreply.github.com>
2025-01-21 22:51:27 -0600

    Brits in YC

--- fraud_detection_app.py
+++ fraud_detection_app.py
@@ -12,7 +12,7 @@ from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-# ycombinator coming soon okie dokie done
+# ycombinator - Irish boy
 
 st.set_page_config(page_title="Credit Card Fraud Detection System", layout="wide")
 
